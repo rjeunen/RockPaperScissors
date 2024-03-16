506347f231bfae8cb869fed76052bb2a89c885a0
Robby Jeunen <robbyjeunen@gmail.com>
2024-03-16 14:34:48 +0100

    added buttons in HTML and all Javascript code for the game

--- script.js
+++ script.js
@@ -13,12 +13,15 @@ function getComputerChoice(){
 //Checking if function getComputerChoice works.
 //console.log(getComputerChoice());
 
+
+/*
 //adding a function to get a choice from the player
+//Function was commented out because of the evenListeners on the added buttons.
 function getPlayerChoice(){
     const playerSelection = prompt("Enter your choice: Rock, Paper or Scissors?: ");
     return playerSelection;
 }
-
+*/
 //adding a function to play a single round.
 function playRound(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
@@ -40,7 +43,84 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+//eventListeners for the new buttons.
+const buttonRock = document.querySelector("#rock");
+const buttonPaper = document.querySelector("#paper");
+const buttonScissors = document.querySelector("#scissors");
+
+let playerScore = 0;
+let computerScore = 0;
+
+const score = document.querySelector("#displayResult");
+
+const div = document.createElement("div");
+div.style.marginTop = "20px";
+div.textContent = `${playerScore} - ${computerScore}`;
+score.appendChild(div);
+
+
+buttonRock.addEventListener("click", setPlayerChoiceRock);
+buttonPaper.addEventListener("click", setPlayerChoicePaper);
+buttonScissors.addEventListener("click", setPlayerChoiceScissors);
+
+function setPlayerChoiceRock(){
+    playerSelection = "rock";
+    let result = playRound("rock", getComputerChoice());
+    if(result.includes("win")){
+        playerScore++;
+    } else{
+        computerScore++;
+    }
+    console.log(result);
+    div.textContent = `${playerScore} - ${computerScore}`;
+    score.appendChild(div);
+    checkScore();
+    div.textContent = `${playerScore} - ${computerScore}`;
+}
+function setPlayerChoicePaper(){
+    playerSelection = "paper";
+    let result = playRound("paper", getComputerChoice());
+    if(result.includes("win")){
+        playerScore++;
+    } else{
+        computerScore++;
+    }
+    console.log(result);
+    div.textContent = `${playerScore} - ${computerScore}`;
+    score.appendChild(div);
+    checkScore();
+    div.textContent = `${playerScore} - ${computerScore}`;
+}
+function setPlayerChoiceScissors(){
+    playerSelection = "scissors";
+    let result = playRound("scissors", getComputerChoice());
+    if(result.includes("win")){
+        playerScore++;
+    } else{
+        computerScore++;
+    }
+    console.log(result);
+    div.textContent = `${playerScore} - ${computerScore}`;
+    score.appendChild(div);
+    checkScore();
+    div.textContent = `${playerScore} - ${computerScore}`;
+}
+function checkScore(){
+    if (playerScore === 5){
+        alert(`You win, score: ${playerScore} - ${computerScore}`);
+        playerScore = 0;
+        computerScore = 0;
+    } else if(computerScore === 5){
+        alert(`You lose, score: ${playerScore} - ${computerScore}`);
+        playerScore = 0;
+        computerScore = 0;
+    }
+}
+
+
+
 //adding a function to play a game. A game is 5 rounds.
+/*
 function playGame(){
     //variables to keep the score
     let playerScore = 0;
@@ -73,17 +153,10 @@ function playGame(){
     console.log(`the score is: Player ${playerScore} VS computer ${computerScore} and the game had ${tieScore} tie rounds`);
     
 
-    /*
-    Hieronder de uitleg zonder loop.
-    Zou je geen loop willen of kunnen gebruiken is het letterlijk de code herhalen.
-    spelerkeuze
-    computerkeuze
-    1ronde
-    de als statement om de punten te tellen
-    REPEAT voor het aantal rondes dat je wilt spelen.
-    */
+
 
 }
+*/
 
 //call function play a game
-playGame();
+//playGame();
